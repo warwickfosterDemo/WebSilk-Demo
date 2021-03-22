25bff572aa00e062a128bd2d1a703802f2211dd5
Warwick Foster <warwick@foster.net>
2021-03-22 20:30:38 +1100

    WebSilk Review

---
+++ WebSilkTestApp.Api.Test/TDD_WebSilkTestApp_Api.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+
+namespace WebSilkTestApp.Api.Test
+{
+    [TestClass]
+    public class TDD_WebSilkTestApp_Api
+    {
+        private string text = "Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea";
+
+        [TestMethod]
+        public void SetupApiSignature()
+        {
+            string text = "";
+            string subtext = "";
+
+            try
+            {
+                List<int> result = Tools.FindPosition(text, subtext);
+            }
+            catch (Exception) { }
+        }
+
+        [TestMethod]
+        public void ParametersCannotBeNull_TextIsNullSoExceptionShouldBeThrown()
+        {
+            string text = null;
+            string subtext = "";
+
+            bool exceptionThrown = false;
+            try
+            {
+                List<int> result = Tools.FindPosition(text, subtext);
+            }
+            catch (NullReferenceException)
+            {
+                exceptionThrown = true;
+            }
+            exceptionThrown.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void ParametersCannotBeNull_SubTextIsNullSoExceptionShouldBeThrown()
+        {
+            string text = "";
+            string subtext = null;
+
+            var exceptionThrown = DoesThrowException(text, subtext);
+            exceptionThrown.ShouldBeTrue();
+        }
+
+        private static bool DoesThrowException(string text, string subtext)
+        {
+            bool exceptionThrown = false;
+            try
+            {
+                List<int> result = Tools.FindPosition(text, subtext);
+            }
+            catch (NullReferenceException)
+            {
+                exceptionThrown = true;
+            }
+            return exceptionThrown;
+        }
+
+        [TestMethod]
+        public void ParametersCannotBeNull_BothAreNullSoExceptionShouldBeThrown()
+        {
+            string text = null;
+            string subtext = null;
+
+            bool exceptionThrown = false;
+            try
+            {
+                List<int> result = Tools.FindPosition(text, subtext);
+            }
+            catch (NullReferenceException)
+            {
+                exceptionThrown = true;
+            }
+            exceptionThrown.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void ParametersCannotBeNull_NeitherAreNullSoNoExceptionShouldBeThrown()
+        {
+            string text = "";
+            string subtext = "";
+
+            bool exceptionThrown = false;
+            try
+            {
+                List<int> result = Tools.FindPosition(text, subtext);
+            }
+            catch (NullReferenceException)
+            {
+                exceptionThrown = true;
+            }
+            exceptionThrown.ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void ReturnResultIsNotNullOnEmptyTest()
+        {
+            string text = "";
+            string subtext = "";
+
+            List<int> result = null;
+            bool exceptionThrown = false;
+            try
+            {
+                result = Tools.FindPosition(text, subtext);
+            }
+            catch (NullReferenceException)
+            {
+                exceptionThrown = true;
+            }
+
+            exceptionThrown.ShouldBeFalse();
+            result.ShouldNotBeNull();
+            result.Count.ShouldEqual(0);
+        }
+
+        [TestMethod]
+        public void PrimaryTestOn_Polly_giving3FoundResultsInCorrectPosition()
+        {
+            string subtext = "Polly";
+
+            List<int> result = Tools.FindPosition(text, subtext);
+
+            result.ShouldNotBeNull();
+            result.Count.ShouldEqual(3);
+            result[0].ShouldEqual(1);
+            result[1].ShouldEqual(26);
+            result[2].ShouldEqual(51);
+        }
+
+        [TestMethod]
+        public void PrimaryTestOn_polly_giving3FoundResultsInCorrectPosition()
+        {
+            string subtext = "polly";
+
+            List<int> result = Tools.FindPosition(text, subtext);
+
+            result.ShouldNotBeNull();
+            result.Count.ShouldEqual(3);
+            result[0].ShouldEqual(1);
+            result[1].ShouldEqual(26);
+            result[2].ShouldEqual(51);
+        }
+
+        [TestMethod]
+        public void PrimaryTestOn_ll_giving5FoundResultsInCorrectPosition()
+        {
+            string subtext = "ll";
+
+            List<int> result = Tools.FindPosition(text, subtext);
+
+            result.ShouldNotBeNull();
+            result.Count.ShouldEqual(5);
+            result[0].ShouldEqual(3);
+            result[1].ShouldEqual(28);
+            result[2].ShouldEqual(53);
+            result[3].ShouldEqual(78);
+            result[4].ShouldEqual(82);
+        }
+
+        [TestMethod]
+        public void PrimaryTestOn_Ll_giving5FoundResultsInCorrectPosition()
+        {
+            string subtext = "Ll";
+
+            List<int> result = Tools.FindPosition(text, subtext);
+
+            result.ShouldNotBeNull();
+            result.Count.ShouldEqual(5);
+            result[0].ShouldEqual(3);
+            result[1].ShouldEqual(28);
+            result[2].ShouldEqual(53);
+            result[3].ShouldEqual(78);
+            result[4].ShouldEqual(82);
+        }
+
+        [TestMethod]
+        public void PrimaryTestOn_polly_giving2FoundResultsWhenCaseInsensitiveIsOff()
+        {
+            string subtext = "polly";
+
+            List<int> result = Tools.FindPosition(text, subtext, false);
+
+            result.ShouldNotBeNull();
+            result.Count.ShouldEqual(2);
+            result[0].ShouldEqual(26);
+            result[1].ShouldEqual(51);
+        }
+
+        [TestMethod]
+        public void PrimaryTestOn_Ll_giving0FoundResultsWhenCaseInsensitiveIsOff()
+        {
+            string subtext = "Ll";
+
+            List<int> result = Tools.FindPosition(text, subtext, false);
+
+            result.ShouldNotBeNull();
+            result.Count.ShouldEqual(0);
+        }
+
+        [TestMethod]
+        public void PrimaryTestOn_X_giving0FoundResultsWithNoError()
+        {
+            string subtext = "X";
+
+            List<int> result = null;
+            bool exceptionThrown = false;
+            try
+            {
+                result = Tools.FindPosition(text, subtext);
+            }
+            catch (NullReferenceException)
+            {
+                exceptionThrown = true;
+            }
+            exceptionThrown.ShouldBeFalse();
+
+            result.ShouldNotBeNull();
+            result.Count.ShouldEqual(0);
+        }
+
+        [TestMethod]
+        public void PrimaryTestOn_polx_giving0FoundResultsWithNoError()
+        {
+            string subtext = "polx";
+
+            List<int> result = null;
+            bool exceptionThrown = false;
+            try
+            {
+                result = Tools.FindPosition(text, subtext);
+            }
+            catch (NullReferenceException)
+            {
+                exceptionThrown = true;
+            }
+            exceptionThrown.ShouldBeFalse();
+
+            result.ShouldNotBeNull();
+            result.Count.ShouldEqual(0);
+        }
+    }
+}
---
+++ WebSilkTestApp.Api/Tools.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebSilkTestApp.Api
+{
+    public static class Tools
+    {
+        private static List<int> result;
+        private static int subTextPosition;
+        private static int foundStart;
+
+        private static string _text;
+        private static string _subtext;
+
+        public static List<int> FindPosition(string text, string subtext, bool caseInsensitive = true)
+        {
+
+            if (text == null || subtext == null)
+                throw new NullReferenceException("Input fields can not be null.");
+
+            _text = text;
+            _subtext = subtext;
+
+            init();
+            setCaseInsensitive(caseInsensitive);
+
+            for (int index = 0; index < text.Length; index++)
+                processCharacter(index);
+            
+            return result;
+        }
+
+        private static void setCaseInsensitive(bool caseInsensitive)
+        {
+            if (caseInsensitive)
+            {
+                _text = _text.ToLower();
+                _subtext = _subtext.ToLower();
+            }
+        }
+
+        private static void init()
+        {
+            result = new List<int>();
+            subTextPosition = 0;
+            foundStart = -1;
+        }
+
+        private static void processCharacter(int index)
+        {
+            char ftChar = _text[index];
+            if (_subtext.Length == subTextPosition)
+                subTextPosition = 0;
+            if (_subtext[subTextPosition] == ftChar)
+            {
+                if (subTextPosition == 0)
+                    setStartIndexForStartOfSubText(index);
+                subTextPosition++;
+                if (_subtext.Length == subTextPosition)
+                    addFoundItemToResultAndReset();
+            }
+            else 
+                ResetFoundStart();
+        }
+
+        private static void setStartIndexForStartOfSubText(int index)
+        {
+            foundStart = index;
+        }
+
+        private static void addFoundItemToResultAndReset()
+        {
+            result.Add(foundStart + 1);
+            ResetFoundStart();
+        }
+
+        private static void ResetFoundStart()
+        {
+            foundStart = -1;
+            subTextPosition = 0;
+        }
+    }
+}
